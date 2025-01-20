2b662189b79cb3da07726dc608556aff8fed70f4
johannaostling <163411394+ostlingjohanna@users.noreply.github.com>
2025-01-20 09:54:20 +0100

    spelling, update css

--- index.js
+++ index.js
@@ -5,70 +5,63 @@ const list = {
         link: "http://tests.imvilabs.com/attention",
         mobil: "",
         dator: "X",
-        pada: "X",
+        padda: "X",
     },
     MapCog: {
         description: "Adminpanel för Siegbet MapCog",
         link: "http://tests.imvilabs.com/attention",
         mobil: "",
         dator: "X",
-        pada: "X",
-    },
-    Readtest1: {
-        description: "Lästest för B/O/M",
-        link: "http://tests.imvilabs.com/lc",
-        mobil: "X",
-        dator: "",
-        pada: "",
+        padda: "X",
     },
     Readtest2: {
         description: "Lästest för Bokstäver",
         link: "http://tests.imvilabs.com/v2/lc",
         mobil: "X",
         dator: "",
-        pada: "",
+        padda: "",
     },
     Readtest3: {
         description: "Lästest för Ord",
         link: "http://tests.imvilabs.com/v2/wc",
         mobil: "X",
         dator: "",
-        pada: "",
+        padda: "",
     },
     Readtest4: {
         description: "Lästest för Meningar",
         link: "http://tests.imvilabs.com/v2/sc",
         mobil: "X",
         dator: "",
-        pada: "",
+        padda: "",
     },
     Readingcomprehension: {
         description: "Läshastighet och Förståelse",
         link: "http://tests.imvilabs.com/readingspeed",
         mobil: "",
         dator: "X",
-        pada: "X",
+        padda: "X",
     },
     Vergensfrågor: {
         description: "Frågor som ställs I appen",
         link: "http://tests.imvilabs.com/vercence",
         mobil: "X",
         dator: "X",
-        pada: "X",
+        padda: "X",
     },
     Amblyopitest1: {
         description: "Testa om man har Amblyopi",
         link: "http://tests.imvilabs.com/amblyopi",
         mobil: "X",
         dator: "X",
-        pada: "X",
+        padda: "X",
     },
     Amblyopitest2: {
         description: "Enkel bild",
         link: "http://tests.imvilabs.com/a.jpg",
         mobil: "X",
         dator: "",
-        pada: "",
+        padda: "",
     },
 };
 
@@ -77,12 +70,12 @@ const entries = Object.entries(list);
 let table = `<table border="1">
 <thead>
 <tr>
-<th>Key</th>
+<th>Test</th>
 <th>Description</th>
 <th>Link</th>
 <th>Mobil</th>
 <th>Dator</th>
-<th>Pada</th>
+<th>Padda</th>
 </tr>
 </thead>
 <tbody>`;
@@ -94,13 +87,36 @@ entries.forEach(([key, value]) => {
         <td><a href="${value.link}">${value.link}</a></td>
         <td>${value.mobil}</td>
         <td>${value.dator}</td>
-        <td>${value.pada}</td>
+        <td>${value.padda}</td>
     </tr>`;
 });
 
 table += "</tbody></table>";
 
 const testlist = document.getElementById("testlist").innerHTML=table
+
+
+document.getElementById("testlist").addEventListener("click", (event)=>{
+   console.log("du tryckte på", event.target.innerHTML)
+   let key = event.target.innerHTML
+   console.log(key)
+   if (list[key])
+    {
+    console.log(list[key].link);
+    navigator.clipboard.writeText(list[key].link).then(() => {
+                console.log("Text kopierad!");
+            }).catch(err => {
+                console.error("Kunde inte kopiera text:", err);
+            });
+    }
+    else{
+        (list[key])
+    }
+
+
+})
+
+
 
 
 
@@ -110,35 +126,35 @@ const coming = {
         link: "",
         mobil: "X",
         dator: "X",
-        pada: "X",
+        padda: "X",
     },
     Vergensfel: {
         description: "Frågor till optikerkunder",
         link: "",
         mobil: "X",
         dator: "X",
-        pada: "X",
+        padda: "X",
     },
     ADHD: {
         description: "Frågor om ADHD/ADD",
         link: "",
         mobil: "X",
         dator: "X",
-        pada: "X",
+        padda: "X",
     },
     COVIDutm: {
         description: "Utmattningsfrågor",
         link: "",
         mobil: "X",
         dator: "X",
-        pada: "X",
+        padda: "X",
     },
-    Koncertration: {
-        description: "Koncertrationsfrågor",
+    Koncentration: {
+        description: "Koncentrationsfrågor",
         link: "",
         mobil: "X",
         dator: "X",
-        pada: "X",
+        padda: "X",
     },
 };
 
@@ -146,12 +162,12 @@ const coming = {
 let comingTable = `<table id='comingTable' border='1'>
 <thead>
 <tr>
-<th>Key</th>
+<th>Test</th>
 <th>Description</th>
 <th>Link</th>
 <th>Mobil</th>
 <th>Dator</th>
-<th>Pada</th>
+<th>Padda</th>
 </tr>
 </thead>
 <tbody>`;
@@ -163,10 +179,11 @@ Object.entries(coming).forEach(([key, value]) => {
         <td><a href="${value.link}">${value.link}</a></td>
         <td>${value.mobil}</td>
         <td>${value.dator}</td>
-        <td>${value.pada}</td>
+        <td>${value.padda}</td>
     </tr>`;
 });
 
 comingTable += "</tbody></table>";
 
 const comingtest= document.getElementById("comingtests").innerHTML= comingTable;
+
